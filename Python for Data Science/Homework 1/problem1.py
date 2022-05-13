1061dcaf3699ce5baa11a059dff862f9619f3117
Miguel Castilho <78663730+MigCast9@users.noreply.github.com>
2022-05-12 21:00:47 -0400

    Add files via upload

---
+++ Python for Data Science/Homework 1/problem1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+day = 'sunday'
+# Your code should be below this line
+if day == 'monday' or day == 'tuesday' or day == 'wednesday' or day == 'thursday' or day == 'friday':
+    print('weekday')
+
+elif day == 'saturday' or day =='sunday':
+    print('weekend')
+
+else:
+    print('neither')
